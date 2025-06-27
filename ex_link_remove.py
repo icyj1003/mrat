0c039afc188e76e7d597a22b2df278e65339567a
Khoi Bui <anhbuikhoi@gmail.com>
2025-06-27 13:07:41 +0900

    update link remove ex

---
+++ ex_link_remove.py
@@ -0,0 +1,11 @@
+cmd = [
+    "python run.py --remove_v2v --name remove_v2v",
+    "python run.py --remove_wifi --name remove_wifi",
+    "python run.py --remove_pc5 --name remove_pc5",
+]
+
+import os
+
+for command in cmd:
+    os.system(command)
+    # print(command)
