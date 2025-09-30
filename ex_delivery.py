dc3bd14687710df11ca74fe085f09f19b9aefbb6
Khoi Bui <anhbuikhoi@gmail.com>
2025-09-30 15:24:28 +0900

    re-organize

--- ex_delivery.py
+++
@@ -1,9 +0,0 @@
-delivery_policy = ["random", "all"]
-
-import os
-
-
-for policy in delivery_policy:
-    cmd = f"python run.py --delivery_policy {policy} --name delivery_{policy} --training_episodes 0 --evaluation_episodes 100"
-    os.system(cmd)
-    # print(cmd)
--- ex_vehicle_scale.py
+++
@@ -1,8 +0,0 @@
-num_vehicles_list = [10, 20, 30, 40, 50]
-
-import os
-
-
-for num_vehicles in num_vehicles_list:
-    cmd = f"python run.py --num_vehicles {num_vehicles} --name vehicle_scale_{num_vehicles}"
-    os.system(cmd)
