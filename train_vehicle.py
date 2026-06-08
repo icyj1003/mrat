7571fc3ff2f58904fc8a4dd75897e4d481727185
Khoi Bui <anhbuikhoi@gmail.com>
2026-06-08 20:35:46 +0900

    update single weight

--- train_vehicle.py
+++ train_vehicle.py
@@ -2,7 +2,7 @@ import os
 
 VEH = [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
 multi_weight = (0.55, 0.45)  # cost - delay
-single_weight = (0.65, 0.45)  # cost - delay
+single_weight = (0.55, 0.45)  # cost - delay
 
 import argparse
 
